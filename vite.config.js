0d2590423c93e28684909dd249eb4d190ee6b174
aaafatik <abdulahadfatik4@gmail.com>
2025-09-15 17:08:20 +0500

    fix issue

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import critical from 'rollup-plugin-critical'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    critical({
-      criticalBase: 'dist/',       // folder where your build output is
-      criticalPages: [
-        { uri: 'index.html', template: 'index.html' } // ✅ correct
-      ],
-      inline: true,                // inline critical CSS into index.html
-      minify: true,                // minify critical CSS
-      extract: true,               // extract remaining CSS to separate file
-    })
-  ],
+  plugins: [react()],
   build: {
-    target: 'esnext',              // modern JS only
-    polyfillDynamicImport: false,  // don’t include extra polyfills
+    target: 'esnext',
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'       // separate vendor bundle
+            return 'vendor'
           }
         }
       }
