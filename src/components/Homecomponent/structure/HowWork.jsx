bfe458289e88ff63fca55593c8d2762033558afc
aaafatik <abdulahadfatik4@gmail.com>
2025-09-23 15:10:02 +0500

    fix

--- src/components/Homecomponent/structure/HowWork.jsx
+++ src/components/Homecomponent/structure/HowWork.jsx
@@ -35,8 +35,6 @@ const HowWork = () => {
                                   ]}
                                 value={activeTab}
                                 onChange={handleTabChange}
-                                aria-label="Select Seller or Buyer workflow"
-                                role="tablist"
                             />
                         </Flex>
 
