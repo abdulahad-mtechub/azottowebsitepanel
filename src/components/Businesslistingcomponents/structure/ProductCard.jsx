9fc15ad39a7137d39c5befc1c389757362d569b0
theasadse <theasadse@gmail.com>
2025-11-03 15:44:20 +0500

    fix: issues

--- src/components/Businesslistingcomponents/structure/ProductCard.jsx
+++ src/components/Businesslistingcomponents/structure/ProductCard.jsx
@@ -182,7 +182,7 @@ const ProductCard = ({
                                                 <React.Fragment key={c}>
                                                     <Col 
                                                         xl={6}
-                                                        lg={8}
+                                                        lg={6}
                                                         md={8}
                                                         sm={12}
                                                         xs={12} 
@@ -191,7 +191,7 @@ const ProductCard = ({
                                                             <Title 
                                                                 level={5} 
                                                                 className='text-brand m-0 fs-13 fw-500'
-                                                                style={{ textAlign: 'center' }} // Ensure text centers
+                                                                style={{ textAlign: 'center' }} 
                                                             >
                                                                 <span className={c !== 2 ? 'currency-display' : ''}>
                                                                     {c !== 2 && 
