f1248bb73cbae8cde76b3082f789f9e0fc82f08a
theasadse <theasadse@gmail.com>
2025-12-26 11:05:58 +0500

    fixed issues

--- src/components/SellBusinessComponents/structure/BusinessInfo.jsx
+++ src/components/SellBusinessComponents/structure/BusinessInfo.jsx
@@ -1,83 +1,96 @@
-import { Card, Col, Flex, Image, Row, Typography } from 'antd'
+import { Card, Col, Flex, Image, Row, Typography } from "antd";
 import { GET_CATEGORY } from "../../../graphql/query/business";
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 
-const { Title, Text } = Typography
-const BusinessInfo = ({data}) => {
-    const { data: categoryData } = useQuery(GET_CATEGORY, {
-        variables: { getCategoryByIdId: data.categoryId },
-        skip: !data.categoryId, // Skip query if categoryId is null or undefined
-      });
-    
-      if (!data.categoryId) {
-        return <p>No category selected.</p>;
-      }
+const { Title, Text } = Typography;
+const BusinessInfo = ({ data }) => {
+  const { data: categoryData } = useQuery(GET_CATEGORY, {
+    variables: { getCategoryByIdId: data.categoryId },
+    skip: !data.categoryId, // Skip query if categoryId is null or undefined
+  });
 
-    const category = categoryData?.getCategoryById;
+  if (!data.categoryId) {
+    return <p>No category selected.</p>;
+  }
 
-    const businessInfoData = [
-        {
-            id: 1,
-            icon:'/assets/icons/businessprice.png',
-            title: <><img src="/assets/icons/reyal-b.png" width={16} alt="currency-symbol" fetchPriority="high" /> {typeof data.price === 'number' ? data.price.toLocaleString() : data.price}</>,
-            subtitle:'Business Price'
-        },
-        {
-            id: 2,
-            icon:'/assets/icons/foundationdate.png',
-            title:`${new Date(data.foundedDate).getFullYear()}`,
-            subtitle:'Foundation Date'
-        },
-        {
-            id: 3,
-            icon:'/assets/icons/businesscate.png',
-            title:`${category?.name}`,
-            subtitle:'Business Category'
-        },
-        {
-            id: 4,
-            icon:'/assets/icons/teamsize.png',
-            title:`${data.numberOfEmployees}`,
-            subtitle:'Team Size'
-        },
-        {
-            id: 5,
-            icon:'/assets/icons/businessloc.png',
-            title:`${data.district}`,
-            subtitle:'Business Location'
-        },
-    ]
+  const category = categoryData?.getCategoryById;
 
-    return (
-        <Card className='shadow-d radius-12 border-gray mb-3'>
-            <Row gutter={[24,24]}>
-                <Col span={24}>
-                    <Title level={5} className='m-0'>
-                        Business Info
-                    </Title>
-                </Col>
-                {
-                    businessInfoData?.map((stat,i)=>
-                        <Col span={24} key={i}>
-                            <Flex gap={10}>
-                                <div className='icon-pre'>
-                                    <Image src={stat?.icon} preview={false} width={'100%'}  alt="stats icon" />
-                                </div>
-                                <Flex vertical gap={2}>
-                                    <Title level={5} className='m-0 text-brand'>
-                                        {stat?.title}
-                                    </Title>
-                                    <Text className='text-gray fs-12 fw-500'>
-                                        {stat?.subtitle}
-                                    </Text>
-                                </Flex>
-                            </Flex>
-                        </Col>
-                    )
-                }
-            </Row>
-        </Card>
-    )
-}
+  const businessInfoData = [
+    {
+      id: 1,
+      icon: "/assets/icons/businessprice.png",
+      title: (
+        <>
+          <img
+            src="/assets/icons/reyal-b.png"
+            width={16}
+            alt="currency-symbol"
+            fetchPriority="high"
+          />{" "}
+          {typeof data.price === "number"
+            ? data.price.toLocaleString()
+            : data.price}
+        </>
+      ),
+      subtitle: "Business Price",
+    },
+    {
+      id: 2,
+      icon: "/assets/icons/foundationdate.png",
+      title: `${new Date(data.foundedDate).getFullYear()}`,
+      subtitle: "Foundation Date",
+    },
+    {
+      id: 3,
+      icon: "/assets/icons/businesscate.png",
+      title: `${category?.name}`,
+      subtitle: "Business Category",
+    },
+    {
+      id: 4,
+      icon: "/assets/icons/teamsize.png",
+      title: `${data.numberOfEmployees}`,
+      subtitle: "Team Size",
+    },
+    {
+      id: 5,
+      icon: "/assets/icons/businessloc.png",
+      title: `${data.district}`,
+      subtitle: "Business Location",
+    },
+  ];
 
-export {BusinessInfo}
+  return (
+    <Card className="shadow-d radius-12 border-gray mb-3">
+      <Row gutter={[24, 24]}>
+        <Col span={24}>
+          <Title level={5} className="m-0">
+            Business Info
+          </Title>
+        </Col>
+        {businessInfoData?.map((stat, i) => (
+          <Col span={24} key={i}>
+            <Flex gap={10}>
+              <div className="icon-pre">
+                <Image
+                  src={stat?.icon}
+                  preview={false}
+                  width={"100%"}
+                  alt="stats icon"
+                />
+              </div>
+              <Flex vertical gap={2}>
+                <Title level={5} className="m-0 text-brand">
+                  {stat?.title}
+                </Title>
+                <Text className="text-gray fs-12 fw-500">{stat?.subtitle}</Text>
+              </Flex>
+            </Flex>
+          </Col>
+        ))}
+      </Row>
+    </Card>
+  );
+};
+
+export { BusinessInfo };
