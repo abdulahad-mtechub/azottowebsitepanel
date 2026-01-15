a03e3953f53e067f287aa7758692885af864f34f
theasadse <theasadse@gmail.com>
2026-01-15 12:10:44 +0500

    fixed issues

--- src/components/Businesslistingcomponents/structure/ProductCard.jsx
+++ src/components/Businesslistingcomponents/structure/ProductCard.jsx
@@ -110,7 +110,7 @@ const ProductCard = ({
         vertical
         justify="center"
         align="center"
-        style={{ width: "100%", minHeight: "400px", padding: "40px 0" }}
+        style={{ width: "100%", height: "50vh", padding: "40px 0" }}
       >
         <img
           src="/assets/icons/info-outline.png"
